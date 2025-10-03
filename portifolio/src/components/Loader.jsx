90e4408ea044c26fdb613c4487ef72b6345d959e
kenenisa1 <kenenisamiesob@gmail.com>
2025-10-03 23:48:01 +0300

    Add a Loader component

--- portifolio/src/components/Loader.jsx
+++ portifolio/src/components/Loader.jsx
@@ -1,10 +1,22 @@
- import React from 'react'
+ import {Html,useProgress} from '@react-three/drei';
+
  
  const Loader = () => {
+  const {progress} = useProgress();
    return (
-     <div>
-        Loader Section
-     </div>
+     <Html>
+        <span className='canvas-load '></span>
+        <p
+          style={{
+            fontSize: 14,
+            color: '#f3f3f3',
+            fontWeight: 800,
+            marginTop: 40,
+          }}
+        >
+          {progress.toFixed (2)}%
+        </p>
+     </Html>
    )
  }
  
