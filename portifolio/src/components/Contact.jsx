1b82608f872266a079534b39eac44365f4b49fe4
kenenisa1 <kenenisamiesob@gmail.com>
2025-10-09 15:21:31 +0300

    Add important functions and form with beautiful styling using tailwindcss

--- portifolio/src/components/Contact.jsx
+++ portifolio/src/components/Contact.jsx
@@ -8,9 +8,35 @@ import { SectionWrapper } from "../hoc"
 import { slideIn } from "../utils/motion" 
 
 const Contact = () => {
+const formRef =useRef();
+const [form, setForm] = useState({
+  name: '',
+  email: '',
+  message: ''
+})
+  const [loading, setLoading] = useState(false)
+;
+const handleChange =( e) => {}
+const handleSubmit = (e) => {}
    return (
-     <div>
-        
+     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex overflow-hidden">
+        <motion.div variants={slideIn('left', "tween" , 0.2 , 1)} className=" flex-[0.75] bg-black-100 p-8 rounded-2xl">
+            <p className={`${styles.heroSubText}`}>GET IN TOUCH</p>
+            <h2 className={`${styles.heroHeadText}`}>Contact</h2>
+        </motion.div>
+
+        <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-12 gap-8">
+          <label 
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+            placeholder="Your name"
+            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+          >
+
+          </label>
+        </form>
      </div>
    )
  }
