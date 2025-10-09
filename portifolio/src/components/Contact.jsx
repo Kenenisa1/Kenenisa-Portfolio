75a025fc0d21cde4d809d6e680589143fc59b7cd
kenenisa1 <kenenisamiesob@gmail.com>
2025-10-09 14:53:10 +0300

    Add Contact page as a component with important file imports

--- portifolio/src/components/Contact.jsx
+++ portifolio/src/components/Contact.jsx
@@ -1,9 +1,16 @@
- import React from 'react'
+import { useState , useRef } from "react"
+import {motion} from 'framer-motion'
+import emailjs from '@emailjs/browser'
  
- const Contact = () => {
+import { styles } from "../styles"
+import { EarthCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { slideIn } from "../utils/motion" 
+
+const Contact = () => {
    return (
      <div>
-        Contact
+        
      </div>
    )
  }
